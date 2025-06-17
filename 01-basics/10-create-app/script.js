a62b9f986323d8a82e476c69d274e4ed1d262ebf
Nikulin Roman <rnikulin@techinfocom.com>
2025-06-17 16:34:00 +0500

    Решение задачи 10-create-app

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,21 @@
 import { defineComponent, createApp } from 'vue'
+
+const App = defineComponent({
+  // Имя компонента - не обязательная опция, но принято иметь - полезно для отладки и редких случаев
+  name: 'App',
+  setup() {
+    const dateFormat = {
+        dateStyle: 'long'
+    };
+    return {
+        dateFormat,
+    }
+  },
+template: `
+    <div class="container">
+        <div>Сегодня: {{ new Date().toLocaleDateString("ru-RU", dateFormat) }}</div>
+    </div>
+  `,
+})
+
+createApp(App).mount('#app')
