7508ee09f11a092e3175461bdf56203a70e3e7c3
Nikulin Roman <rnikulin@techinfocom.com>
2025-06-19 17:10:50 +0500

    Решение задачи 10-create-app (remove WeatherApp)

--- 01-basics/20-weather/main.js
+++
@@ -1,6 +0,0 @@
-import '@shgk/vue-course-ui/meetups/style.css'
-import './weather.css'
-import { createApp } from 'vue'
-import WeatherApp from './WeatherApp.js'
-
-createApp(WeatherApp).mount('#app')
