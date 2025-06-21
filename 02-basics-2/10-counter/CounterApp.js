c152369326d74ac4414f5c90b662e968ed0c1719
Nikulin Roman <rnikulin@techinfocom.com>
2025-06-21 22:45:14 +0500

    Решение задачи 10-counter

--- 02-basics-2/10-counter/CounterApp.js
+++ 02-basics-2/10-counter/CounterApp.js
@@ -1,9 +1,47 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref, watch } from 'vue'
 
 export default defineComponent({
   name: 'CounterApp',
 
-  setup() {},
+  setup() {
+    const count = ref(0)
+    const increment = () => {
+      if (count.value >= 5) {
+        return;
+      }
+      count.value += 1
+    }
+    const decrement = () => {
+      if (count.value <= 0) {
+        return;
+      }
+      count.value -= 1
+    }    
+ 
+    watch(count, (newValue) => {
+      if (newValue < 0) {
+        count.value = 0
+      } else if (newValue > 5) {
+        count.value = 5
+      }
+    });
+
+    return {
+      count,
+      increment,
+      decrement,
+    }
+  },
+
+  // watch: {
+  //   count(newValue) {
+  //     if (newValue < 0) {
+  //       this.count = 0
+  //     } else if (newValue > 5) {
+  //       this.count = 5
+  //     }
+  //   },
+  // },
 
   template: `
     <div class="counter">
@@ -11,15 +49,18 @@ export default defineComponent({
         class="button button--secondary"
         type="button"
         aria-label="Decrement"
-        disabled
+        @click="decrement"
+        :disabled="count <= 0"
       >➖</button>
 
-      <span class="count" data-testid="count">0</span>
+      <span class="count" data-testid="count">{{ count }}</span>
 
       <button
         class="button button--secondary"
         type="button"
         aria-label="Increment"
+        @click="increment"
+        :disabled="count >= 5"
       >➕</button>
     </div>
   `,
