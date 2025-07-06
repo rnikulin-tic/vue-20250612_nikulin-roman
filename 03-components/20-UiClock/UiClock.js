63e7823f5f035d6da788f9181f263b0ee4bf5f07
Nikulin Roman <rnikulin@techinfocom.com>
2025-07-06 18:28:02 +0500

    Решение задачи 20-UiClock

--- 03-components/20-UiClock/UiClock.js
+++ 03-components/20-UiClock/UiClock.js
@@ -1,9 +1,39 @@
-import { defineComponent } from 'vue'
+import { onMounted, onUnmounted, defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'UiClock',
 
-  setup() {},
+  setup() {
+    const dateFormat = {
+      timeStyle: 'medium',
+    }
+    const currentTime = ref(formatTime(new Date()))
+    let interval = null
 
-  template: `<div class="clock">10:12:02</div>`,
+    function formatTime(date) {
+      try {
+        return date.toLocaleTimeString(navigator.language, dateFormat)
+      } catch (e) {
+        return date.toLocaleTimeString()
+      }
+    }
+
+    onMounted(() => {
+      interval = setInterval(() => {
+        currentTime.value = formatTime(new Date())
+      }, 1000)
+    })
+
+    onUnmounted(() => {
+      if (interval) {
+        clearInterval(interval)
+      }
+    })
+
+    return {
+      currentTime,
+    }
+  },
+
+  template: `<div class="clock">{{ currentTime }}</div>`,
 })
