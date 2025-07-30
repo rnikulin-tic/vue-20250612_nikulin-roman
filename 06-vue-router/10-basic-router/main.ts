da389a02f556de4a01cf5f30a588a3e02ff1dd66
Nikulin Roman <rnikulin@techinfocom.com>
2025-07-30 21:23:55 +0500

    Решение 10-basic-router

--- 06-vue-router/10-basic-router/main.ts
+++ 06-vue-router/10-basic-router/main.ts
@@ -1,6 +1,6 @@
 import '@shgk/vue-course-ui/style.css'
 import { createApp } from 'vue'
 import App from './App.vue'
-// import { router } from './router/router.ts'
+import { router } from './router/router.ts'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
--- 06-vue-router/10-basic-router/views/PageLogin.vue
+++ 06-vue-router/10-basic-router/views/PageLogin.vue
@@ -4,14 +4,19 @@ import { ref } from 'vue'
 import MeetupsAuthForm from '../components/MeetupsAuthForm.vue'
 import LayoutAuth from '../components/LayoutAuth.vue'
 import { login } from '../api.ts'
+import { useRoute, useRouter } from 'vue-router'
 
 const email = ref('demo@email')
 const password = ref('password')
+
+const route = useRoute();
+const router = useRouter();
 
 async function onSubmit() {
   try {
     await login(email.value, password.value)
     // Авторизация прошла успешно
+     router.push(!route. query.from ? '/' : ('' + route. query.from));
   } catch (error) {
     alert((error as Error).message)
   }
@@ -35,7 +40,7 @@ async function onSubmit() {
 
       <template #append>
         Нет аккаунта?
-        <a href="/register">Зарегистрируйтесь</a>
+        <RouterLink to="/register">Зарегистрируйтесь</RouterLink>
       </template>
     </MeetupsAuthForm>
   </LayoutAuth>
