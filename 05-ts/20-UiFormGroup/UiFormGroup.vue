edacd29d6217f32175a612467b1d0114c3451ae6
Nikulin Roman <rnikulin@techinfocom.com>
2025-07-30 16:54:00 +0500

    Решение 20-UiFormGroup

--- 05-ts/20-UiFormGroup/UiFormGroup.vue
+++ 05-ts/20-UiFormGroup/UiFormGroup.vue
@@ -1,13 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { Slot } from 'vue';
+
+
+defineProps<{
+  for?: string,
+  label?: string,
+  description?: string,
+  hint?: string,
+  showHint?: boolean,
+  invalid?: boolean
+}>()
+
+defineSlots<{
+  default?: Slot,
+  label?: Slot,
+  description?: Slot
+}>()
+
+</script>
 
 <template>
   <div class="form-group">
     <div class="form-group__label-wrapper">
-      <label for="FOR" class="form-group__label">LABEL</label>
-      <div class="form-group__description">DESCRIPTION</div>
+      <label :for="$props.for" class="form-group__label">
+        <slot v-if="$slots.label" name="label"></slot>
+        <p v-else>{{ label }}</p>
+      </label>
+      <div class="form-group__description">
+        <slot v-if="$slots.description" name="description"></slot>
+        <p v-else>{{ description }}</p>
+      </div>
     </div>
-    <div class="form-group__control">CONTENT</div>
-    <div class="form-group__hint form-group__hint--invalid">HINT | ERROR</div>
+    <div class="form-group__control">
+      <slot />
+    </div>
+    <div v-if="hint !== undefined" :class="{ 'form-group__hint': true, 'form-group__hint--invalid': invalid }">
+      <p v-if="showHint || invalid">{{ hint }}</p>
+    </div>
   </div>
 </template>
 
